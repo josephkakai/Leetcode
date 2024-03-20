ec8552a110c64327fd1905b05efd2d45180b37c7
Kaks <josephngalu96@gmail.com>
2024-03-20 09:32:20 +0300

    70. Climbing Stairs

---
+++ 70._Climbing Stairs.js/70._Climbing Stairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 0) return 0;
+
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+};
+
+console.log(climbStairs(3)); // 2
